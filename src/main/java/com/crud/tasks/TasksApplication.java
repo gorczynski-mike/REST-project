fac76312f9b36de2509e6e1d82a1f55dc7d0fd87
Mike <gorczynski.mike@gmail.com>
2018-06-17 21:21:01 +0200

    added few components and get task by id

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -2,7 +2,9 @@ package com.crud.tasks;
 
 import com.crud.tasks.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class TasksApplication {
--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,9 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,14 +13,19 @@ import java.util.List;
 @RequestMapping("/v1/task")
 public class TaskController {
 
+    @Autowired
+    private DbService service;
+    @Autowired
+    private TaskMapper taskMapper;
+
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        return taskMapper.mapToTaskDtoList(service.getAllTasks());
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask/{taskId}")
     public TaskDto getTask(@PathVariable Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
+        return taskMapper.mapToTaskDto(service.findTaskById(taskId));
     }
 
     @DeleteMapping(value = "deleteTask/{taskId}")
@@ -26,12 +34,12 @@ public class TaskController {
     }
 
     @PutMapping(value = "updateTask")
-    public TaskDto updateTask(@RequestParam("taskDto") TaskDto taskDto) {
+    public TaskDto updateTask(@RequestBody TaskDto taskDto) {
         return new TaskDto(1L, "Edited test title", "test_content");
     }
 
     @PostMapping(value = "createTask")
-    public void createTask(@RequestParam("taskDto") TaskDto taskDto) {
+    public void createTask(@RequestBody TaskDto taskDto) {
         //TO DO
     }
 
