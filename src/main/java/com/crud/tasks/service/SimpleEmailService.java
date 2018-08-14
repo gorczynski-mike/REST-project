e0972868df7f69f11ee57e2f043398ca4f722651
Mike <gorczynski.mike@gmail.com>
2018-08-14 23:20:22 +0200

    added thymeleaf mail template to the project

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,26 +19,26 @@ public class SimpleEmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Autowired
+    private MailCreatorService mailCreatorService;
+
     public void send(Mail mail) {
         LOGGER.info("Starting: sending new email.");
         try {
-            SimpleMailMessage simpleMailMessage = createMailMessage(mail);
-            javaMailSender.send(simpleMailMessage);
+            javaMailSender.send(createMimeMessage(mail));
             LOGGER.info("Mail sent successfully.");
         } catch (MailException e) {
             LOGGER.error("Failed to process sending new email: ", e.getMessage(), e);
         }
     }
 
-    private SimpleMailMessage createMailMessage(Mail mail) {
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setTo(mail.getMailTo());
-        simpleMailMessage.setSubject(mail.getSubject());
-        simpleMailMessage.setText(mail.getMessage());
-        if (mail.getToCc() != null && !mail.getToCc().equals("")) {
-            simpleMailMessage.setCc(mail.getToCc());
-        }
-        return simpleMailMessage;
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
     }
 
 }
