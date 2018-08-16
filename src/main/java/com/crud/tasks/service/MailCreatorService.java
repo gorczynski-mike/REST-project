6b02388e9e48e68a6262bd8b7a759403a02f26b3
Mike <gorczynski.mike@gmail.com>
2018-08-16 21:49:02 +0200

    modified new trello card email template

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -22,11 +25,21 @@ public class MailCreatorService {
     private Environment env;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with trello account");
+        functionality.add("Application allows sending tasks to trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend/");
         context.setVariable("button", "Visit website");
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
         context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("application_functionality", functionality);
         context.setVariable("goodbye_message", "Feel free to search on Google in case of any problems, bye.");
         context.setVariable("company_name", env.getProperty("info.company.name"));
         context.setVariable("company_email", env.getProperty("info.company.email"));
